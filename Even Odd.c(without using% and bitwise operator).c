c68cdfd23bde035436050675f3fba00be97d485d
amanugi <46647548+amanugi@users.noreply.github.com>
2019-01-30 00:36:22 +0530

    Add files via upload

---
+++ Even Odd.c(without using% and bitwise operator).c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter any no");
+    scanf("%d",&n);
+
+     if((n/2)*2==n)
+        printf("An even number");
+     else
+        printf("An odd no");
+     return(0);
+}
