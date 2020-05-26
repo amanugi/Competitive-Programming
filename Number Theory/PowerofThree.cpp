710575c727039f66aeda1b5114cc63af428a040a
Aman Singh <46647548+amanugi@users.noreply.github.com>
2020-05-26 10:06:14 +0530

    Create PowerofThree.cpp

---
+++ Number Theory/PowerofThree.cpp
@@ -0,0 +1,12 @@
+bool isPowerOfThree(int n) {
+        if(n == 0)
+            return false;
+        
+        while(n%3 == 0)
+            n = n/3;
+        if(n==1)
+            return true;
+        else{
+            return false;
+        }
+    }
